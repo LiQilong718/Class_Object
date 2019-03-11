b6c5ec911f73a07672dbd442815e35649598dc26
liqilong718 <13673729700@163.com>
2019-03-11 18:35:41 +0800

    Initial commit

---
+++ src/DogTestDrive.java
@@ -0,0 +1,9 @@
+public class DogTestDrive {
+    public static void main(String[] args){
+        //Dog 测试码
+        Dog d=new Dog();//建立Dog对象
+        //通过原点运算符调用它的方法
+        d.size=40;
+        d.bark();
+    }
+}
